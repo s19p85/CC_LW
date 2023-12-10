6dd79e528c4317d965962806465326e01d54935d
s19p85 <levmyskin75@gmail.com>
2023-12-11 00:15:52 +0400

    edit table script

--- src/create.table.php
+++ src/create.table.php
@@ -2,23 +2,25 @@
 //Подключится к базе данных
 require_once "connect.pg.inc.php";
 //Если такая таблица есть, удалить ее
-$sql = "DROP TABLE IF EXISTS catalog CASCADE";
+$sql = "DROP TABLE IF EXISTS cars CASCADE";
 $pdo->query($sql);
 
 //Создание таблицы
 $sql = "
-CREATE TABLE catalog (
+CREATE TABLE cars (
     id SERIAL PRIMARY KEY,
-    author TEXT NOT NULL,
-    title TEXT NOT NULL,
-    pubyear INT NOT NULL,
-    price float
+    model TEXT NOT NULL,
+    color TEXT NOT NULL,
+    horsepower float NOT NULL,
+    speed float NOT NULL
 )";
 
 $pdo->query($sql);
 //Занесение данных
 $sql = "
-INSERT INTO catalog VALUES (1,'Jon', 'PHP',2020,500)";
+INSERT INTO cars VALUES (1, 'Porsche 911', 'Рубиновый', 480, 330), (2, 'NISSAN GT-R', 'Белый', 419, 315),
+(3, 'Audi R8', 'Графитовый', 540, 320), (4, 'ВАЗ 2107', 'Изумрудный', 74, 150), (5, 'Lancer Evo X', 'Охра золотистая', 295, 242)
+";
 $pdo->query($sql);
 //Закрыть подключение к базе данных
 $pdo = null;
--- src/index.php
+++ src/index.php
@@ -1,29 +1,29 @@
 <html>
 
 <head>
-    <title>Каталог товаров</title>
+    <title>Каталог машин</title>
 </head>
 
 <body>
-    <h2>Каталог товаров</h2>
-    <table border="0" cellpadding="5" cellspacing="0" width="100%">
+    <h2>Каталог машин</h2>
+    <table border="1" cellpadding="5" cellspacing="1" width="100%">
         <tr>
-            <th>Автор</th>
-            <th>Название</th>
-            <th>Год издания</th>
-            <th>Цена, руб.</th>
+            <th>Модель</th> 
+            <th>Цвет</th>
+            <th>Кол-во лошадиных сил</th>
+            <th>Макс скорость, км/ч</th>
         </tr>
         <?php
             require_once "connect.pg.inc.php";
-            $sql = "SELECT * FROM catalog";
+            $sql = "SELECT * FROM cars";
             $sta = $pdo->query($sql);
             while ($row = $sta->fetch(PDO::FETCH_ASSOC)) {
         ?>
         <tr>
-            <td><?php echo $row["author"] ?></td>
-            <td><?php echo $row["title"] ?></td>
-            <td align="center"><?php echo $row["pubyear"]?></td>
-            <td align="center"><?php echo $row["price"]?></td>
+            <td><?php echo $row["model"] ?></td>
+            <td><?php echo $row["color"] ?></td>
+            <td align="center"><?php echo $row["horsepower"]?></td>
+            <td align="center"><?php echo $row["speed"]?></td>
         </tr>
         <?php
             }
